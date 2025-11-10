dd1159aa91844703ae198183309ee2d035c9c65e
user <user@WIN-CCU0G7VSS60>
2025-11-10 15:47:51 +0900

    プロジェクトのアップ

---
+++ java_sample/src/lesson24/c/Phone2401.java
@@ -0,0 +1,15 @@
+package lesson24.c;
+
+/**
+ * 携帯電話クラス
+ */
+public class Phone2401 {
+    /** 料金 */
+    private int fee;
+    /** データ通信量 */
+    private double data;
+
+    public void method() {
+    	System.out.println(fee + data);
+    }
+}
