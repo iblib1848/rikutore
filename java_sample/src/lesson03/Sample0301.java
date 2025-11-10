a3d93805ff3e18b047ddb8a71af466dc84117401
user <user@WIN-CCU0G7VSS60>
2025-11-10 15:49:43 +0900

    最初の問題

--- java_sample/src/lesson03/Sample0301.java
+++ java_sample/src/lesson03/Sample0301.java
@@ -3,5 +3,6 @@ package lesson03;
 public class Sample0301 {
     public static void main(String[] args) {
         System.out.println("ようこそJavaへ！");
+        System.out.println("ようこそJavaへ！");
     }
 }
